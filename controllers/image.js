eceabf814b08365b299feaec6073ffe3539085c8
DevDaveJ <dajoyce.br@gmail.com>
2024-04-02 01:44:35 +0100

    Detect face debug

--- controllers/image.js
+++ controllers/image.js
@@ -41,7 +41,9 @@ const handleClarifaiCall = (req, res) => {
 			// We'll get one WorkflowResult for each input we used above. Because of one input, we have here
 			// one WorkflowResult.
 			const results = response.results[0];
-
+			console.log({ results });
+			res.json(results);
+			
 			// Each model we have in the workflow will produce one output.
 			for (const output of results.outputs) {
 				const model = output.model;
