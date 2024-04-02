19ee8d61a496e378f1abaf848411a97a50d826a5
DevDaveJ <dajoyce.br@gmail.com>
2024-04-03 00:11:22 +0100

    Detect face debug

--- controllers/image.js
+++ controllers/image.js
@@ -69,7 +69,7 @@ const handleClarifaiCall = (req, res) => {
 			});
 			console.log("Regions object: ",{ regions });
 
-			return res.json(JSON.stringify(regions));
+			res.json(regions);
 		}
 	);
 }
